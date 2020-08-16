d1c47d9418b1e493b647c11b2998d16baccf1041
shikaiheng <874716257@qq.com>
2020-08-16 10:54:01 +0800

    增加了一条输出语句，并需改了之前的输出语句。

--- src/main/java/com/hangzhou/app/App.java
+++ src/main/java/com/hangzhou/app/App.java
@@ -2,6 +2,7 @@ package com.hangzhou.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("123");
+        System.out.println("125");
+        System.out.println("456");
     }
 }
