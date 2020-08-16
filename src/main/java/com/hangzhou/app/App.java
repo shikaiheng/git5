d700b34fb05cbb1215d8dd772d317d5be361ee2a
shikaiheng <51788509+shikaiheng@users.noreply.github.com>
2020-08-16 20:57:48 +0800

    Update App.java

--- src/main/java/com/hangzhou/app/App.java
+++ src/main/java/com/hangzhou/app/App.java
@@ -3,6 +3,6 @@ package com.hangzhou.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("125");
-       // System.out.println("456");
+        System.out.println("456");
     }
 }
