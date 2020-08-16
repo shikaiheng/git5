3abe921efc716219ddef865486be853a50d6c2b6
shikaiheng <51788509+shikaiheng@users.noreply.github.com>
2020-08-16 21:23:17 +0800

    Update App.java
    aaa

--- src/main/java/com/hangzhou/app/App.java
+++ src/main/java/com/hangzhou/app/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("125");
         System.out.println("456");
+        System.out.println("789");
+         int a = 10;
     }
 }
